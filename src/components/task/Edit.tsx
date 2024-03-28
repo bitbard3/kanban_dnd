36884a31a5e98f9a16f3cc60eaf24c4bf462ed41
bitbard3 <ansharora3839@gmail.com>
2024-03-29 05:21:56 +0530

    Add task edit component

---
+++ src/components/task/Edit.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { HiMiniEllipsisVertical } from "react-icons/hi2";
+
+export default function Edit() {
+  return (
+    <button className="">
+      <HiMiniEllipsisVertical className=" text-neutral-900 h-5 w-5" />
+    </button>
+  );
+}
