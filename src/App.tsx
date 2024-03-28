37fad5b73461569a7a2f6ec07afe91fcd93776e2
bitbard3 <ansharora3839@gmail.com>
2024-03-28 19:58:22 +0530

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Kanban from "./pages/Kanban";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Kanban />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Kanban.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Kanban() {
+  return (
+    <div>
+      <div className="bg-yellow h-screen w-screen"></div>
+    </div>
+  );
+}
