7c47e89caac9264b2038670f0342b67f4af755d9
bitbard3 <ansharora3839@gmail.com>
2024-03-29 05:29:48 +0530

    Add footer component

---
+++ src/components/task/Footer.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { LuCalendar } from "react-icons/lu";
+
+type FooterProps = {
+  assignee: string;
+  date: string;
+};
+
+export default function Footer({ assignee, date }: FooterProps) {
+  return (
+    <div className="flex w-full items-center mt-4">
+      <p className="text-sm text-[#35227b] font-medium">@{assignee}</p>
+      <div className="flex items-center  space-x-1.5 ml-auto mr-1.5">
+        <LuCalendar className="text-black" />
+        <p className="text-black text-opacity-60 font-medium text-xs pt-0.5">
+          {date}
+        </p>
+      </div>
+    </div>
+  );
+}
