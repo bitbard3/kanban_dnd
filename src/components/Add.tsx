e351b656bf08835a250ca8fd04e67c50ad26efcc
bitbard3 <ansharora3839@gmail.com>
2024-03-29 07:06:29 +0530

    Add new task component

---
+++ src/components/Add.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Add() {
+  return (
+    <button className="text-white bg-green rounded-lg hover:opacity-95 px-10 py-1.5 text-lg">
+      Add Task
+    </button>
+  );
+}
