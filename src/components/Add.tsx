26c0a5c1c17f9ed5e6e02aa66ba0aa683a9bb875
bitbard3 <ansharora3839@gmail.com>
2024-03-30 22:18:00 +0530

    Create dialog trigger in add button

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import DialogBody from "./DialogBody";
 
 export default function Add() {
   return (
-    <button className="text-white bg-green rounded-lg hover:opacity-95 px-10 py-1.5 text-lg">
-      Add Task
-    </button>
+    <Dialog>
+      <DialogTrigger>
+        <div className="text-white bg-green rounded-lg hover:opacity-95 px-10 py-1.5 text-lg">
+          Add Task
+        </div>
+      </DialogTrigger>
+      <DialogBody />
+    </Dialog>
   );
 }
