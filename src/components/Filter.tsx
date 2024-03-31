4179cc2f6b170c51d4515ed700334bfa9fb4fe21
bitbard3 <ansharora3839@gmail.com>
2024-03-31 21:28:58 +0530

    Add filter stores

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { LuListFilter } from "react-icons/lu";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import FilterPopover from "./FilterPopover";
 
 export default function Filter() {
   return (
-    <div className="px-2 py-2 bg-green rounded-full flex items-center justify-center hover:opacity-95">
-      <button>
-        <LuListFilter color="#fff" className="h-6 w-6" />
-      </button>
-    </div>
+    <Popover>
+      <PopoverTrigger>
+        <div className="px-2 py-2 bg-green rounded-full flex items-center justify-center hover:opacity-95">
+          <button>
+            <LuListFilter color="#fff" className="h-6 w-6" />
+          </button>
+        </div>
+      </PopoverTrigger>
+      <PopoverContent>
+        <FilterPopover />
+      </PopoverContent>
+    </Popover>
   );
 }
