bdc592da47ba04bef59dad9f52c059ed09c0081c
bitbard3 <ansharora3839@gmail.com>
2024-04-01 06:54:17 +0530

    remove diferent list and maintain one list of task

--- src/stores/atoms/task.ts
+++ src/stores/atoms/task.ts
@@ -1,26 +1,7 @@
 import { Task } from "@/interface/task";
 import { atom } from "recoil";
 
-export const pending = atom({
-  key: "pending",
-  default: [] as Task[],
-});
-
-export const progress = atom({
-  key: "progress",
-  default: [] as Task[],
-});
-
-export const completed = atom({
-  key: "completed",
-  default: [] as Task[],
-});
-
-export const deployed = atom({
-  key: "deployed",
-  default: [] as Task[],
-});
-export const deferred = atom({
-  key: "deferred",
+export const taskAtom = atom({
+  key: "taskAtom",
   default: [] as Task[],
 });
